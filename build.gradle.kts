5ccd9fcce65388a8dce285ea75e7d1c97e0cf2db
Michael Kunze <mkunze@inoxio.de>
2021-05-17 11:17:18 +0200

    Remove deps that are pulled in by spring-test

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,9 +32,7 @@ dependencies {
 
     // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.mockito:mockito-core:3.9.0")
     testImplementation("nl.jqno.equalsverifier:equalsverifier:3.6")
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
 }
 
 java {
